162f39c4377f74ce976d7c403837ae9c14307711
ayxan386 <ayxanpro@gmail.com>
2020-08-02 22:42:58 +0400

    added get by id end point

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,5 +1,19 @@
 package controllers
 
-class UserController {
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
+import play.api.mvc.{AbstractController, ControllerComponents}
+import repository.UserRepository
 
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class UserController @Inject() (cc: ControllerComponents, userRepository: UserRepository)(implicit ex : ExecutionContext)
+extends AbstractController(cc){
+
+  def getById(id: Long) = Action.async {
+    request =>
+      userRepository.getById(id)
+        .map(Ok(Json.toJson(_)))
+  }
 }
