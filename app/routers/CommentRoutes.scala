dc9fd7763f4c27bc3f98f5914f80b627984fce4e
ayxan386 <ayxanpro@gmail.com>
2020-08-16 23:01:03 +0400

    post API get methods modified

---
+++ app/routers/CommentRoutes.scala
@@ -0,0 +1,14 @@
+package routers
+
+import controllers.CommentController
+import javax.inject.{Inject, Singleton}
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird._
+
+@Singleton
+class CommentRoutes @Inject()(cc : CommentController) extends SimpleRouter{
+  override def routes: Routes = {
+    case POST(p"/") => cc.addComment()
+  }
+}
--- app/services/impl/PostServiceImpl.scala
+++ app/services/impl/PostServiceImpl.scala
@@ -5,14 +5,15 @@ import error.notfound.{PostNotFound, UserNotFound}
 import javax.inject.{Inject, Singleton}
 import models.{Post, User}
 import repository.{PostRepository, UserRepository}
-import services.PostService
+import services.{CommentService, PostService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class PostServiceImpl @Inject()(
     postRepository: PostRepository,
-    userRepository: UserRepository)(implicit ex: ExecutionContext)
+    userRepository: UserRepository,
+    commentService: CommentService)(implicit ex: ExecutionContext)
     extends PostService {
 
   override def save(dto: PostDTO,
@@ -46,36 +47,31 @@ class PostServiceImpl @Inject()(
   override def getAll(): Future[List[Future[PostDTO]]] = {
     postRepository
       .getAll()
-      .map(
-        list =>
-          list.map(
-            model =>
-              userRepository
-                .getById(model.userId)
-                .map(user => mapUserModelToResponse(user))
-                .map(userResponse =>
-                  PostDTO(title = model.title,
-                          body = model.body,
-                          user = Some(userResponse),
-                          comments = Nil))))
+      .map(list => list.map(fromModel))
   }
 
   override def getById(id: Long): Future[PostDTO] = {
     postRepository
       .getById(id)
       .map(op => op.getOrElse(throw PostNotFound()))
+      .map(fromModel)
+      .flatMap(f => f)
+  }
+
+  private def fromModel(model: Post) = {
+    userRepository
+      .getById(model.userId)
+      .map(opU => opU.map(u => UserResponse(u.userDB, u.role, u.addresses)))
       .map(
-        model =>
-          userRepository
-            .getById(model.userId)
-            .map(opU =>
-              opU.map(u => UserResponse(u.userDB, u.role, u.addresses)))
-            .map(
-              opU =>
-                PostDTO(title = model.title,
-                        body = model.body,
-                        user = opU,
-                        comments = Nil)))
+        opU =>
+          PostDTO(title = model.title,
+                  body = model.body,
+                  user = opU,
+                  comments = Nil))
+      .map(dto =>
+        commentService
+          .getAllByPostId(model.id)
+          .map(cl => dto.copy(comments = cl)))
       .flatMap(f => f)
   }
 
